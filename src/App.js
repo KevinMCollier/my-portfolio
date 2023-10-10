a3b49eaec24dd2061e045146fc3e5ba36f6133a1
kevin collier <collier.kevin.m@gmail.com>
2023-10-10 20:26:52 +0900

    copy-pasted boiler template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "./components/About";
+import Banner from ".components/Banner";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
+      <Banner />
       <About />
       <Projects />
       <Skills />
