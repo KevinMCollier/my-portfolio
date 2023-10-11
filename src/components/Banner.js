392a1580e17534d7f5c04a21c68f6e8c8b77bc57
kevin collier <collier.kevin.m@gmail.com>
2023-10-11 20:00:23 +0900

    removed links from banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,18 +14,6 @@ export default function Banner() {
             laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
             Laborum, voluptas natus?
           </p>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded text-lg">
-              Work With Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-yellow-300 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-400 hover:text-white rounded text-lg">
-              See My Past Work
-            </a>
-          </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
