405c9cebd2df478970399c4cfe575017c4061527
kevin collier <collier.kevin.m@gmail.com>
2023-10-11 23:05:39 +0900

    removed google map and added map component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import About from "./components/About";
+import Map from "./components/Map"
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Projects />
       <About />
       <Contact />
+      <Map />
     </main>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from "react";
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+export default function Map() {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    // Initialize the map
+    const map = new mapboxgl.Map({
+      container: mapContainer.current, // Container element
+      style: 'mapbox://styles/mapbox/streets-v11', // Style URL
+      center: [139.7216, 35.6281], // Longitude and latitude of your favorite restaurant
+      zoom: 15 // Zoom level
+    });
+
+    // Add navigation control (zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    // Add a marker at the specified location
+    new mapboxgl.Marker()
+      .setLngLat([139.7216, 35.6281])
+      .addTo(map);
+
+    // Clean up on component unmount
+    return () => map.remove();
+  }, []);
+
+
+  return (
+
+    <section id="map" className="relative">
+      <div ref={mapContainer} style={{ width: '50%', height: '300px', margin: 'auto' }}></div>
+    </section>
+  );
+}
