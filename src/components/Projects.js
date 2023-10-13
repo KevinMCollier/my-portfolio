de9c3fec96167fe5d109ee2b81d45d8b706d10f3
kevin collier <collier.kevin.m@gmail.com>
2023-10-14 08:43:18 +0900

    added icons and updated links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,22 +15,32 @@ export default function Projects() {
         <div className="flex flex-col items-start -m-4">
           {projects.map((project) => (
             <div key={project.image} className="flex flex-col-reverse sm:flex-row w-full sm:p-8 p-4 sm:border-b border-gray-300">
-              <a href={project.link}>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
                 <img
                   alt="gallery"
-                  className="w-full object-cover object-center shadow-xl"
+                  className="w-full object-cover object-center shadow-2xl"
                   style={{ height: '250px', width: '1000px' }}
                   src={project.image}
                 />
               </a>
               <div className="sm:ml-8 flex-row mb-6">
-                <h2 className="sm:text-xl sm:text-left mb-2 text-2xl font-semibold">{project.title}</h2>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                  <h2 className="sm:text-xl sm:text-left mb-2 text-2xl font-semibold hover:text-cyan-700 uppercase tracking-wider">{project.title}</h2>
+                </a>
                 <p className="text-left mb-6">
                   {project.description}
                 </p>
                 <div className="flex flex-wrap">
-                  {project.stack.map((skill, index) =>(
-                    <p className="text-left inline-block mr-2">{skill}{index !== project.stack.length - 1 ? ' |' : ''}</p>
+                  {project.stack.map((tech, index) =>(
+                    <div className="sm:text-left mr-3 flex items-center">
+                      <div className="hover:bg-yellow-200 flex items-center">
+                        <img src={tech.logo} alt={tech.name} className="w-4 h-4 mr-1" />
+                        <p>{tech.name}</p>
+                      </div>
+                      <div className="ml-2">
+                        {index !== project.stack.length - 1 ? ' |' : ''}
+                      </div>
+                    </div>
                   ))}
                 </div>
               </div>
