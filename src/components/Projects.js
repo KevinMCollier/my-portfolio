4901f969c607318d1a8870763e5d07f25b317e6d
kevin collier <collier.kevin.m@gmail.com>
2023-10-13 22:45:24 +0900

    fixed alignment for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,57 +1,37 @@
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
+import { Fragment } from "react";
 
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-800 bg-gray-100 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-col w-full mb-20">
+        <div className="flex flex-col w-full mb-5">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-800">
             Projects
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            {/* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore. */}
-          </p>
         </div>
-        <div className="flex flex-col flex-start items-start -m-4">
+        <div className="flex flex-col items-start -m-4">
           {projects.map((project) => (
-
-
-
-
-
-
-
-
-
-
-
-
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
+            <div key={project.image} className="flex flex-col-reverse sm:flex-row w-full sm:p-8 p-4 sm:border-b border-gray-300">
+              <a href={project.link}>
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center shadow-xl"
+                  className="w-full object-cover object-center shadow-xl"
+                  style={{ height: '250px', width: '1000px' }}
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
+              </a>
+              <div className="sm:ml-4 flex-row mb-6">
+                <h2 className="sm:text-xl sm:text-left mb-2 text-2xl font-semibold">{project.title}</h2>
+                <p className="text-left">
+                  {project.description}
+                </p>
               </div>
-            </a>
+              <br></br>
+            </div>
           ))}
         </div>
       </div>
