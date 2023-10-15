57a8f77a45970e745e49aa7996c721d335ebcc85
kevin collier <collier.kevin.m@gmail.com>
2023-10-15 22:42:44 +0900

    updated banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,13 +6,14 @@ export default function Banner() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Hi, I'm Kevin.
-            <br className="hidden lg:inline-block" /> I love to build amazing apps.
+            I'm Kevin.
+            <br className="hidden lg:inline-block" /> I build apps that make life a piece of cake 🍰
           </h1>
+          <p className="mb-1 leading-relaxed">
+            Chances are I'm also eating a piece of cake.
+          </p>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            When I'm not coding, you can find me at one of my favorite restaurants across Tokyo.
           </p>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
