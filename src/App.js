7fac4fe3e38cb4ad0382ae7421a2232868b630ba
kevin collier <collier.kevin.m@gmail.com>
2023-10-15 13:26:02 +0900

    removed import map from app js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import About from "./components/About";
-import Map from "./components/Map"
 
 export default function App() {
   return (
